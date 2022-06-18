cc93326dfe2574312437b4228c69aac1d76a5f14
cpaschall <cpaschall216@gmail.com>
2022-06-18 14:06:56 -0400

    added to projectRoutes.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,44 +1,44 @@
-const {Model, DataTypes} = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
-class Comment extends Model {}
+// const {Model, DataTypes} = require('sequelize');
+// const bcrypt = require('bcrypt');
+// const sequelize = require('../config/connection');
+// class Comment extends Model {}
 
-Comment.init({
-    id:{
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement:true,
-    },
-    commentData:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [1]
-        }
-    },
-    user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
-    },
-    post_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'post',
-            key: 'id'
-        }
-    }, 
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'comment',
+// Comment.init({
+//     id:{
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//         primaryKey: true,
+//         autoIncrement:true,
+//     },
+//     commentData:{
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         validate: {
+//             len: [1]
+//         }
+//     },
+//     user_id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//         references: {
+//             model: 'user',
+//             key: 'id'
+//         }
+//     },
+//     post_id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//         references: {
+//             model: 'post',
+//             key: 'id'
+//         }
+//     }, 
+//         sequelize,
+//         timestamps: false,
+//         freezeTableName: true,
+//         underscored: true,
+//         modelName: 'comment',
     
     
-})
-modeule.exports = Comment;
+// })
+// modeule.exports = Comment;
