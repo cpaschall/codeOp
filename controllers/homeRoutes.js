73f72e7f5ea6e586ac0e691eeb6dd510690a8c4e
cpaschall <cpaschall216@gmail.com>
2022-06-19 21:29:07 -0400

    refactoring user, project and home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,26 +1,58 @@
 const router = require('express').Router();
-const { Project, User } = require('../models')
-
-const users = [
-    {
-        name: "tester",
-        email: "test@email.com",
-        password: "1234",
-
-    }
-]
+const { Project, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    // const userData = await User.findAll({
-    //     include: [
-    //         {
-    //             model: User,
-    //             attributes: ['name'],
-    //         },
-    //     ],
-    // });
-    // const users = userData.map((user) => user.get({ plain: true }))
-    res.render('homepage', users[0]);
+    try {
+        const projectData = await Project.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                },
+            ],
+        });
+
+        const projects = projectData.map((project) => project.get({ plain: true }));
+
+        res.render('homepage', {
+            projects,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.statusMessage(500).json(err);
+    }
+});
+
+router.get('/project/:id', async (req, res) => {
+    try {
+        const projectData = await Project.findByPk(req.params.id, {
+        include: [
+            {
+            model: User,
+            attributes: ['name'],
+            },
+        ],
+        });
+
+        const project = projectData.get({ plain: true });
+
+        res.render('project', {
+        ...project,
+        logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+}
+
+res.render('login');
 });
 
 module.exports = router;
