b8d83207c34dc933bf94db84ec67a28d69c46c9d
Vincent Tjia <vtjia51@gmail.com>
2022-06-21 20:21:08 -0400

    add editprofile js and handlebar

---
+++ public/js/editprofile.js
@@ -0,0 +1,31 @@
+
+const editProfileHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector("#edit-name").value.trim();
+    const email = document.querySelector("edit-email").value.trim();
+    const skills = document.querySelector("#skills").value.trim();
+
+
+    if (name && email && skills) {
+        const response = await fetch('/api/editprofile', {
+            method: "PUT",
+            body: JSON.stringify({ name, email, skills }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace("/profile");
+        }
+        else {
+            alert("Error! Cannot update.");
+        }
+    }
+
+};
+
+document
+  .querySelector('.edit-update')
+  .addEventListener('submit', editProfileHandler);
