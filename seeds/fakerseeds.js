1dd9b81d21ecc9c2ec5ea0ff11723f5159baa5d4
cpaschall <cpaschall216@gmail.com>
2022-06-24 15:59:02 -0400

    refactoring fakerseeds.js

--- seeds/fakerseeds.js
+++ seeds/fakerseeds.js
@@ -13,37 +13,52 @@ const createRandomProject = () => {
 
     for(var i = 0; i <= 5; i++) {
         let randProj = {
+            id: i+1,
             proj_name: faker.music.songName(),
             summary: faker.company.catchPhrase(),
             language: ["HTML", "JavaScript", "CSS", "MySql", "React"]
         }
-        allUsers.push(randProj)
+        allProjects.push(randProj)
     }
-    return allUsers 
+    return allProjects
+    console.log(allProjects)
 };
 
 const createRandomUser = () => {
     // let randNum = Math.floor(Math.random() * skills.length);
 
-    for(var i = 0; i < allProjects.length; i++) {
+    for(var i = 0; i < 5; i++) {
         let randUser = {
+            id: i+1,
             name: faker.internet.userName(),
             email: faker.internet.email(),
             password: faker.internet.password(),
             skills: ["HTML", "JavaScript", "CSS", "MySql", "React"],
-            proj_id: i+1,
+            proj_id: i+1
         }
         allUsers.push(randUser)
     }
-    return allUsers 
+    return allUsers
+    // console.log(allUsers)
+    // console.log({
+    //     name: faker.internet.userName(),
+    //     email: faker.internet.email(),
+    //     password: faker.internet.password(),
+    //     skills: ["HTML", "JavaScript", "CSS", "MySql", "React"],
+    //     proj_id: i+1
+    // })
+    
 };
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
-  createRandomProject();
-  createRandomUser();
-  
+//   createRandomProject();
+//   createRandomUser();
+    // createRandomUser()
+    createRandomProject()
+    createRandomUser()
+
     const users = await User.bulkCreate(allUsers, {
       individualHooks: true,
       returning: true,
@@ -55,9 +70,15 @@ const seedDatabase = async () => {
         user_id: users[Math.floor(Math.random() * users.length)].id,
       });
     }
+    console.log(allUsers)
+    console.log(allProjects)
   
     process.exit(0);
   };
   
-  seedDatabase();
+
+// createRandomUser()
+// createRandomProject()
+// createRandomUser()
+seedDatabase();
   
