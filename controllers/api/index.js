5539008eccee2fe6597be41df06fdc4f19e414c1
Vincent Tjia <vtjia51@gmail.com>
2022-06-24 23:52:32 -0400

    commented index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoute');
 const projectRoutes = require('./projectRoutes')
-const commentRoutes = require('./commentRoute')
+// const commentRoutes = require('./commentRoute')
 // const seedDatabaseRoutes = require("./seedRoute")
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
-router.use('/comments', commentRoutes);
+// router.use('/comments', commentRoutes);
 // router.use('/seeds', seedDatabaseRoutes);
 
 module.exports = router;
